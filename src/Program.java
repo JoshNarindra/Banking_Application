4318f7fa254e6d479310efa3c88342f875dd793c
Josh <jbhawanlall@gmail.com>
2022-12-28 00:40:40 +0000

    Added Menu Framework

--- src/Program.java
+++ src/Program.java
@@ -11,13 +11,15 @@ public class Program {
     }
 
     public static void accountExists(){
-        //Initial Menu
-        System.out.println("Welcome to ACME banking solutions...");
+        //Initial Menu.
+        System.out.println("Welcome to ACME Banking Solutions...");
         System.out.println("Does the customer currently have an account with us? \n 1. Yes. \n 2. No.");
 
+        //Scanner to read bank tellers input for user choice.
         Scanner s1 = new Scanner(System.in);
         int menu = s1.nextInt();
 
+        //Switch-Case statement for initial menu.
         switch (menu) {
             case 1 -> customerInfo();
             case 2 -> createAccount();
@@ -40,12 +42,27 @@ public class Program {
         System.out.println("D.O.B: ");
 
         //Display customers accounts with bank.
+        System.out.println("Customer Accounts:");
 
+        //Option to allow current customer to open new account.
 
     }
 
     public static void createAccount(){
+        //Menu to create an account for new customer.
+        System.out.println("Open an account with ACME Banking");
+        System.out.println(" 1. Open a personal account. \n 2. Open a business account. \n 3. Open a ISA Account2");
 
+        //Scanner to read bank tellers input for user menu choice.
+        Scanner s3 = new Scanner(System.in);
+        int menu = s3.nextInt();
+
+        //Switch-Case statement for initial menu.
+        switch (menu) {
+            case 1 -> System.out.println("Placeholder for personal account creation");
+            case 2 -> System.out.println("Placeholder for business account creation");
+            case 3 -> System.out.println("Placeholder for ISA account creation");
+        }
     }
 
 
