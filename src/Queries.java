1518d539fcad08df0d5c4d7a7d2b8ebfc5d30ab8
ewanhughes26 <79140539+ewanhughes26@users.noreply.github.com>
2023-01-12 17:19:17 +0000

    Corrected IntelliJ-reported problems in Queries class

--- src/Queries.java
+++ src/Queries.java
@@ -1,6 +1,24 @@
-//Separate class for all queries which need to be executed before an instance of an Account is created
-//For example cannot call functions inside of Account, BusinessAccount etc. until a query made is made to create an instance
-//Also separates the queries from the connection itself which looks more tidy I think
+/*
+    Class Queries is responsible for executing all SQL queries made by the application.
+
+    The three main methods in the class take SQL queries as arguments and execute the queries, returning results where applicable:
+        updateQuery(),
+        readQuery(),
+        checkExistsQuery().
+
+    Retrieve/check methods call the readQuery() or checkExistsQuery() methods and return relevant information:
+        retrievePersonalAccount(),
+        retrieveBusinessAccount(),
+        retrieveISAAccount(),
+        retrieveBusinessName(),
+        checkAccountExists().
+
+    Create methods call the updateQuery() method to alter database and the readQuery() method to return relevant information:
+        createUser(),
+        createPersonalAccount(),
+        createBusinessAccount(),
+        createISAAccount().
+ */
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -12,13 +30,12 @@ public class Queries
     public ArrayList<String> usersColumns = new ArrayList<>(List.of("ID", "FirstName", "LastName", "DateOfBirth"));
     public ArrayList<String> businessesColumns = new ArrayList<>(List.of("ID", "Name", "AccountNumber"));
 
-    //Method which runs update queries on the database
-    public static void updateQuery(String query) throws SQLException
+    // Method updateQuery() takes a String query as an argument and executes the query in the SQL database.
+    public static void updateQuery(String query)
     {
         try
         {
-            DatabaseConnection connection = new DatabaseConnection();
-            var statement = connection.getConnection().prepareStatement(query);
+            var statement = DatabaseConnection.getConnection().prepareStatement(query);
             statement.execute();
         }
         catch (Exception e)
@@ -27,23 +44,22 @@ public class Queries
         }
     }
 
-    //Method which runs read queries on the database
-    //Returns results an ArrayList of strings which contain the results from the table
-    //If connection fails null is returned
-    public static ArrayList<String> readQuery(String query, ArrayList columnNames) throws SQLException
+    // Method readQuery() takes a String query and an ArrayList of Strings columnNames as arguments.
+    // The query is executed on the SQL database.
+    // An ArrayList is then returned containing results of columnNames obtained from executing the query.
+    public static ArrayList<String> readQuery(String query, ArrayList<String> columnNames)
     {
         ArrayList<String> results = new ArrayList<>();
         try
         {
-            DatabaseConnection connection = new DatabaseConnection();
-            var statement = connection.getConnection().prepareStatement(query);
+            var statement = DatabaseConnection.getConnection().prepareStatement(query);
             var resultSet = statement.executeQuery();
 
             while (resultSet.next())
             {
-                for (int i = 0; i < columnNames.size(); i++)
+                for (String columnName : columnNames)
                 {
-                    results.add(resultSet.getString(columnNames.get(i).toString()));
+                    results.add(resultSet.getString(columnName));
                 }
             }
 
@@ -56,24 +72,19 @@ public class Queries
         return results;
     }
 
+    // Method checkExistsQuery() takes a String query as an argument.
+    // The query is executed on the SQL database.
+    // A boolean true is then returned if the query returns results.
     public static boolean checkExistsQuery(String query)
     {
         try
         {
-            DatabaseConnection connection = new DatabaseConnection();
-            var statement = connection.getConnection().prepareStatement(query);
+            var statement = DatabaseConnection.getConnection().prepareStatement(query);
             var resultSet = statement.executeQuery();
 
-            while(resultSet.next())
+            if (resultSet.next())
             {
-                if (resultSet.getInt(1) == 1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return resultSet.getInt(1) == 1;
             }
         }
         catch (SQLException e)
@@ -83,79 +94,76 @@ public class Queries
         return false;
     }
 
-    // Method queries database to check if an account number exists in database. Returns true if account exists and false otherwise. -CHECK IF CAN USE PREEXISTING METHOD.
+    // Method checkAccountExists() takes a String accountNumber as an argument and checks if the account number is in use in the database.
+    // A boolean true is then returned if the account number already exists.
     public boolean checkAccountExists(String accountNumber)
     {
         return checkExistsQuery("SELECT COUNT(1) FROM Accounts0 WHERE AccountNumber = '" + accountNumber + "';");
     }
 
-    //Method takes the account number provided and creates a PersonalAccount object using information from the database
-    public PersonalAccount retrievePersonalAccount(String accountNumber) throws SQLException
+    // Method retrievePersonalAccount() takes a String accountNumber as an argument and creates a PersonalAccount object using information from the database.
+    // This PersonalAccount object is then returned.
+    public PersonalAccount retrievePersonalAccount(String accountNumber)
     {
         ArrayList<String> accountInformation;
         accountInformation = readQuery("SELECT * FROM Accounts0 WHERE AccountNumber = '" + accountNumber + "';", accountsColumns);
-        PersonalAccount personalAccount = new PersonalAccount(accountInformation.get(0), accountInformation.get(1), Float.parseFloat(accountInformation.get(4)), Float.parseFloat(accountInformation.get(5)));
-        return personalAccount;
+        return new PersonalAccount(accountInformation.get(0), accountInformation.get(1), Float.parseFloat(accountInformation.get(4)), Float.parseFloat(accountInformation.get(5)));
     }
 
-    //Method takes the account number provided and creates an ISAAccount object using information from the database
-    public ISAAccount retrieveISAAccount(String accountNumber) throws SQLException
+    // Method retrieveISAAccount() takes a String accountNumber as an argument and creates an ISAAccount object using information from the database.
+    // This ISAAccount object is then returned.
+    public ISAAccount retrieveISAAccount(String accountNumber)
     {
         ArrayList<String> accountInformation;
         accountInformation = readQuery("SELECT * FROM Accounts0 WHERE AccountNumber = '" + accountNumber + "';", accountsColumns);
-        ISAAccount isaAccount = new ISAAccount(accountInformation.get(0), accountInformation.get(1), Float.parseFloat(accountInformation.get(4)), Float.parseFloat(accountInformation.get(5)));
-        return isaAccount;
+        return new ISAAccount(accountInformation.get(0), accountInformation.get(1), Float.parseFloat(accountInformation.get(4)), Float.parseFloat(accountInformation.get(5)));
     }
 
-    //Method takes the account number provided and creates a BusinessAccount object using information from the database
-    public BusinessAccount retrieveBusinessAccount(String accountNumber) throws SQLException
+    // Method retrieveBusinessAccount takes a String account number as an argument and creates a BusinessAccount object using information from the database.
+    // This BusinessAccount object is then returned.
+    public BusinessAccount retrieveBusinessAccount(String accountNumber)
     {
         ArrayList<String> accountInformation;
         accountInformation = readQuery("SELECT * FROM Accounts0 WHERE AccountNumber = '" + accountNumber + "';", accountsColumns);
-        BusinessAccount businessAccount = new BusinessAccount(accountInformation.get(0), accountInformation.get(1), Float.parseFloat(accountInformation.get(4)), Float.parseFloat(accountInformation.get(5)), retrieveBusinessName(accountNumber));
-        return businessAccount;
+        return new BusinessAccount(accountInformation.get(0), accountInformation.get(1), Float.parseFloat(accountInformation.get(4)), Float.parseFloat(accountInformation.get(5)), retrieveBusinessName(accountNumber));
     }
 
-    public String retrieveBusinessName(String accountNumber) throws SQLException
+    // Method retrieveBusinessName() takes a String accountNumber as an argument and returns the business name associated with the account as a String.
+    public String retrieveBusinessName(String accountNumber)
     {
         ArrayList<String> businessInformation;
         businessInformation = readQuery("SELECT * FROM Businesses0 WHERE AccountNumber = '" + accountNumber + "';", businessesColumns);
-        String businessName = businessInformation.get(1);
-        return businessName;
+        return businessInformation.get(1);
     }
 
-    //Method takes the name and date of birth of a person, creates a user in the Users table, and returns the UserID of that user
-    //This UserID can then be used as an argument for the createAccount method
-    public int createUser(String firstName, String lastName, String dateOfBirth) throws SQLException
+    // Method createUser() takes the name and date of birth of a person, creates a user in the Users table, and returns the UserID of that user.
+    // This UserID can then be used as an argument for other methods.
+    public int createUser(String firstName, String lastName, String dateOfBirth)
     {
         updateQuery("INSERT INTO Users0 (FirstName, LastName, DateOfBirth) VALUES ('" + firstName + "', '" + lastName + "', '" + dateOfBirth + "');");
         ArrayList<String> userInformation = readQuery("SELECT * FROM Users0 WHERE (FirstName = '" + firstName + "' AND LastName = '" + lastName + "' AND DateOfBirth = '" + dateOfBirth + "');", usersColumns);
-        int userID = Integer.parseInt(userInformation.get(0));
-        return userID;
+        return Integer.parseInt(userInformation.get(0));
     }
 
-    //Method takes the relevant information and creates a personal account, before returning the account itself as an object
-    public PersonalAccount createPersonalAccount(String accountNumber, String sortCode, int userID, float balance, float overdraft) throws SQLException
+    // Method createPersonalAccount() takes the relevant information and creates a personal account, before returning the account itself as a PersonalAccount object.
+    public PersonalAccount createPersonalAccount(String accountNumber, String sortCode, int userID, float balance, float overdraft)
     {
         updateQuery("INSERT INTO Accounts0 VALUES ('" + accountNumber + "', '" + sortCode + "', '" + userID + "', '" + "Personal" + "', " + balance + ", " + overdraft + ");");
-        PersonalAccount personalAccount = retrievePersonalAccount(accountNumber);
-        return personalAccount;
+        return retrievePersonalAccount(accountNumber);
     }
 
-    //Method takes the relevant information and creates an ISA account, before returning the account itself as an object
-    public ISAAccount createISAAccount(String accountNumber, String sortCode, int userID, float balance, float overdraft) throws SQLException
+    // Method createISAAccount() takes the relevant information and creates an ISA account, before returning the account itself as an ISAAccount object.
+    public ISAAccount createISAAccount(String accountNumber, String sortCode, int userID, float balance, float overdraft)
     {
         updateQuery("INSERT INTO Accounts0 VALUES ('" + accountNumber + "', '" + sortCode + "', " + userID + ", '" + "ISA" + "', " + balance + ", " + overdraft + ");");
-        ISAAccount isaAccount = retrieveISAAccount(accountNumber);
-        return isaAccount;
+        return retrieveISAAccount(accountNumber);
     }
 
-    //Method takes the relevant information and creates a business account, before returning the account itself as an object
-    public BusinessAccount createBusinessAccount(String accountNumber, String sortCode, int userID, float balance, float overdraft, String businessName) throws SQLException
+    // Method createBusinessAccount() takes the relevant information and creates a business account, before returning the account itself as a BusinessAccount object.
+    public BusinessAccount createBusinessAccount(String accountNumber, String sortCode, int userID, float balance, float overdraft, String businessName)
     {
         updateQuery("INSERT INTO Accounts0 VALUES ('" + accountNumber + "', '" + sortCode + "', " + userID + ", '" + "Business" + "', " + balance + ", " + overdraft + ");");
         updateQuery("INSERT INTO Businesses0 (Name, AccountNumber) VALUES ('" + businessName + "', '" + accountNumber + "');");
-        BusinessAccount businessAccount = new BusinessAccount(accountNumber, sortCode, balance, overdraft, businessName);
-        return businessAccount;
+        return new BusinessAccount(accountNumber, sortCode, balance, overdraft, businessName);
     }
 }
